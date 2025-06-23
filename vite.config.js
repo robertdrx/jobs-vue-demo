be398b94c3fa19a642ad610e139d09f85208bb39
Robert Stroe <stroe_d_valentin@yahoo.com>
2025-06-23 22:28:54 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://json-server-fy39.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }],
+  },
+})
